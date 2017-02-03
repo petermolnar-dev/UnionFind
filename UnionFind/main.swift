13d80c2d009a2f69b53e70d945eb936a9b1daf92
Peter Molnar <peter@petermolnar.hu>
2017-02-03 18:13:17 +0100

    Initial Commit

---
+++ UnionFind/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  UnionFind
+//
+//  Created by Peter Molnar on 03/02/2017.
+//  Copyright © 2017 Peter Molnar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
